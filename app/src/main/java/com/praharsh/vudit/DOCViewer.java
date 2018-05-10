f64eabcccbc9fa3f6fd90590706ba77e7c6eafa3
Praharsh Jain <praharsh@sportskeeda.com>
2018-05-11 00:43:15 +0530

    move webview code to common activity and refactor code

--- app/src/main/java/com/praharsh/vudit/DOCViewer.java
+++ app/src/main/java/com/praharsh/vudit/DOCViewer.java
@@ -1,57 +1,13 @@
 package com.praharsh.vudit;
 
-import android.graphics.Bitmap;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.WindowManager;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 import java.net.URLEncoder;
 
-public class DOCViewer extends AppCompatActivity {
-    WebView wv;
-
+public class DOCViewer extends WebViewBaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.webview);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
-                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
-        wv = (WebView) findViewById(R.id.browser);
-        wv.setWebViewClient(new MyClient());
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            // chromium, enable hardware acceleration
-            wv.setLayerType(View.LAYER_TYPE_HARDWARE, null);
-        } else {
-            // older android version, disable hardware acceleration
-            wv.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-        }
-        wv.setWebChromeClient(new WebChromeClient() {
-            public void onProgressChanged(WebView view, int progress) {
-            }
-        });
-        WebSettings settings = wv.getSettings();
-        settings.setSupportZoom(true);
-        settings.setDisplayZoomControls(true);
-        settings.setJavaScriptEnabled(true);
-        settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
-        settings.setDomStorageEnabled(true);
-        settings.setRenderPriority(WebSettings.RenderPriority.HIGH);
-        settings.setLoadWithOverviewMode(true);
-        settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
-        settings.setDomStorageEnabled(true);
-        settings.setAllowFileAccess(true);
-        settings.setAllowContentAccess(true);
-        settings.setAllowFileAccessFromFileURLs(true);
-        settings.setAllowUniversalAccessFromFileURLs(true);
-        wv.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
-        wv.setScrollbarFadingEnabled(true);
         String file_path = getIntent().getStringExtra("file");
         boolean isPDF = getIntent().getBooleanExtra("isPDF", false);
         try {
@@ -62,34 +18,5 @@ public class DOCViewer extends AppCompatActivity {
             wv.loadUrl("file:///android_asset/pdfviewer/web/viewer.html?file=" + file_path);
         else
             wv.loadUrl("file:///android_asset/docviewer/index.html?file=" + file_path);
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK) {
-            if (wv.canGoBack()) {
-                wv.goBack();
-                return false;
-            }
-        }
-        return super.onKeyDown(keyCode, event);
-    }
-
-    class MyClient extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return super.shouldOverrideUrlLoading(view, url);
-        }
-
-        @Override
-        public void onPageStarted(WebView view, String url, Bitmap favicon) {
-            super.onPageStarted(view, url, favicon);
-        }
-
-        @Override
-        public void onPageFinished(WebView view, String url) {
-            super.onPageFinished(view, url);
-        }
     }
 }
--- app/src/main/java/com/praharsh/vudit/TextViewer.java
+++ app/src/main/java/com/praharsh/vudit/TextViewer.java
@@ -1,62 +1,21 @@
 package com.praharsh.vudit;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.WindowManager;
 import android.webkit.JavascriptInterface;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.net.URLEncoder;
 
-public class TextViewer extends AppCompatActivity {
-    WebView wv;
+public class TextViewer extends WebViewBaseActivity {
     String file_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.webview);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
-                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
-        wv = (WebView) findViewById(R.id.browser);
-        wv.setWebViewClient(new MyClient());
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            // chromium, enable hardware acceleration
-            wv.setLayerType(View.LAYER_TYPE_HARDWARE, null);
-        } else {
-            // older android version, disable hardware acceleration
-            wv.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-        }
-        wv.setWebChromeClient(new WebChromeClient() {
-            public void onProgressChanged(WebView view, int progress) {
-            }
-        });
-        WebSettings settings = wv.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
-        settings.setDomStorageEnabled(true);
-        settings.setRenderPriority(WebSettings.RenderPriority.HIGH);
-        settings.setLoadWithOverviewMode(true);
-        settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
-        settings.setDomStorageEnabled(true);
-        settings.setAllowFileAccess(true);
-        settings.setAllowContentAccess(true);
-        settings.setAllowFileAccessFromFileURLs(true);
-        settings.setAllowUniversalAccessFromFileURLs(true);
-        wv.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
-        wv.setScrollbarFadingEnabled(true);
         String file_path = getIntent().getStringExtra("file");
         File file = new File(Uri.parse(file_path).getPath());
         try {
@@ -78,35 +37,6 @@ public class TextViewer extends AppCompatActivity {
             wv.loadUrl("javascript:showFile('" + file_text + "')");
         } catch (Exception e) {
             e.printStackTrace();
-        }
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK) {
-            if (wv.canGoBack()) {
-                wv.goBack();
-                return false;
-            }
-        }
-        return super.onKeyDown(keyCode, event);
-    }
-
-    class MyClient extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return super.shouldOverrideUrlLoading(view, url);
-        }
-
-        @Override
-        public void onPageStarted(WebView view, String url, Bitmap favicon) {
-            super.onPageStarted(view, url, favicon);
-        }
-
-        @Override
-        public void onPageFinished(WebView view, String url) {
-            super.onPageFinished(view, url);
         }
     }
 
